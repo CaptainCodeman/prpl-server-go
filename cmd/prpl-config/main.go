67f4801d85e22fa4589fb5e88bb7f7725a2e200c
Simon Green <simon@captaincodeman.com>
2017-08-18 21:45:06 -0600

    Use go 1.6
    Iterating files is currently broken, again, for Go 1.8 / Appengine
    Chi router was replaced with std lib because it required 1.8 or higher

--- cmd/prpl-config/main.go
+++ cmd/prpl-config/main.go
@@ -108,7 +108,7 @@ const templateString = `{{ if .ProjectID }}project: {{ .ProjectID }}{{ end }}
 {{ if .ProjectVersion }}version: {{ .ProjectVersion }}{{ end }}
 service: {{ .ProjectService }}
 runtime: go
-api_version: go1.8
+api_version: go1.6
 
 instance_class: F1
 
